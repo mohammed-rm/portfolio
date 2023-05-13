fec58d3ecff21e1db440769fbd3df2ba5f2bf75d
Mohammed R <mohammed.rmich@etu.univ-tours.fr>
2023-05-13 16:00:07 +0200

    go top page icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,9 +5,10 @@ import Hero from "../components/Hero";
 import About from "../components/About";
 import WorkExperience from "../components/WorkExperience";
 import Skills from "../components/Skills";
-import GoUPButton from "../components/GoUPButton";
 import Projects from "../components/Projects";
 import Contact from "../components/Contact";
+import Link from "next/link";
+import {ArrowUpCircleIcon} from "@heroicons/react/24/solid";
 
 const Home: NextPage = () => {
     return (
@@ -19,8 +20,6 @@ const Home: NextPage = () => {
             </Head>
 
             <Header/>
-
-            <GoUPButton/>
 
             <section id="hero" className="snap-start">
                 <Hero/>
@@ -45,6 +44,13 @@ const Home: NextPage = () => {
             <section id="contact" className="snap-start">
                 <Contact/>
             </section>
+
+            <Link href={"#hero"}>
+                <footer className="fixed bottom-10 right-10 z-20">
+                    <ArrowUpCircleIcon className="h-10 w-10 text-slate-400 hover:text-[#F7AB0A] cursor-pointer"/>
+                </footer>
+            </Link>
+
         </div>
     );
 };
