4cea5fc14e7930cd762d807cdb8ab4578e93397e
Mohammed R <mohammed.rmich@etu.univ-tours.fr>
2023-05-18 12:41:42 +0200

    utility functions to fetch data

---
+++ utils/fetchPageInfos.ts
@@ -0,0 +1,10 @@
+import {PageInfo} from "../typings";
+
+export const fetchPageInfos = async () => {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/page-infos`);
+
+    const data = await res.json();
+    const pageInfo: PageInfo[] = data.pageInfo;
+
+    return pageInfo;
+}
